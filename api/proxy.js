19dfe0c1498dfeb99bcfa836036a41d16012cfbd
polytropo <primoz969@gmail.com>
2023-05-09 11:29:30 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,19 +1,14 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-  const apiUrl = req.query.apiUrl;
-  const event_id = req.query.event_id;
+  const { apiUrl } = req.query;
+  const newUrl = new URL(apiUrl);
 
+  newUrl.searchParams.set('token', '154761-g9sYpS0kbXfwrV');
   try {
-    const response = await axios.get(apiUrl, {
-      params: {
-        token: '154761-g9sYpS0kbXfwrV',
-        ...(event_id && { event_id }),
-      },
-    });
-
+    const response = await axios.get(newUrl.toString());
     res.status(200).json(response.data);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching data from the API' });
+    res.status(500).json({ error: 'Unable to fetch data from the API' });
   }
 };
