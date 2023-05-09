869be1e4848df5f98a8e387aac56dbfdac746ae2
Primoz <primoz969@gmail.com>
2023-05-09 21:50:36 +0200

    refactors code so it looks cleaner

--- src/components/APICall.jsx
+++ src/components/APICall.jsx
@@ -24,138 +24,231 @@ const APICall = () => {
 		const filteredResults = filterData(filtersSelected, showFor === 'world' ? allWorldMatches : allUSAMatches)
 		setMatchesToShow(filteredResults)
 	}, [filtersSelected])
- 
-	const updateData = async () => {
-		axios.get('/api/proxy?apiUrl=https://api.b365api.com/v3/events/inplay&sport_id=18')
-		.then((res) => {
-			if (res?.data?.results) {
-				const filledWorldData = [];
-				const filledUSAData = [];
-				const fetchedData = res?.data?.results;
-				// Move all events to state
-				// or do another call now for each of them
-				// For each of them sent the even view
-				fetchedData.forEach((singleData, index) => {
-					axios.get(`/api/proxy?apiUrl=https://api.b365api.com/v1/event/view&event_id=${singleData.id}`)
-						.then((eventData) => {
-							if (eventData?.data?.results[0]) {
-								const detailedData = eventData.data.results[0];
-								if (Object.keys(detailedData.stats).length && Object.keys(detailedData.timer).length && detailedData.league.id !== '25067' && detailedData.league.id !== '23105') {
-									console.log('Adding for id: ', detailedData.id, detailedData)
-									// If USA country then push in separate array.
-									// Add only if stats and timer objects are not empty
-									const scoreArr = (detailedData.ss).split('-')
-									const convertedObject = {
-										homeName: detailedData.home.name,
-										awayName: detailedData.away.name,
-										country: detailedData.league.cc,
-										score: detailedData.ss,
-										scoreDiff: parseInt(scoreArr[0]) - parseInt(scoreArr[1]),
-										scoreTotal: parseInt(scoreArr[0]) + parseInt(scoreArr[1]),
-										bet365Odds: { handicap: null },
-										bet365OddsHandicap: null,
-										timer: detailedData.timer,
-										timerString: `${detailedData.timer.tm}min ${detailedData.timer.ts}sec`,
-										//  ${detailedData.timer.tt} -> check what it means
-										stats: detailedData.stats,
-										id: detailedData.id,
-										key: detailedData.id,
-										// quarter: `${detailedData.timer.q} ${(detailedData.extra.numberofperiods !== '4' ? '2H' : '')}`,
-										quarter: `${detailedData.timer.q}`,
-										foulsHome: detailedData.stats.fouls[0],
-										foulsAway: detailedData.stats.fouls[1],
-										points2Home: detailedData.stats['2points'][0],
-										points2Away: detailedData.stats['2points'][1],
-										points3Home: detailedData.stats['3points'][0],
-										points3Away: detailedData.stats['3points'][1],
-										freeThrowsHome: detailedData.stats.free_throws[0],
-										freeThrowsAway: detailedData.stats.free_throws[1],
-										freeThrowsRateHome: detailedData.stats.free_throws_rate[0],
-										freeThrowsRateAway: detailedData.stats.free_throws_rate[1],
-										timeoutsHome: detailedData.stats.time_outs[0],
-										timeoutsAway: detailedData.stats.time_outs[1],
-										// numberofperiods: detailedData.extra.numberofperiods,
-									}
-									// if (index === 0 || index === 1) {
-									// 	axios.get(`https://api.b365api.com/v2/event/odds?token=154761-g9sYpS0kbXfwrV&event_id=${convertedObject.id}`)
-									// 	.then((dataBack) => {
-									// 		console.log('Databack:2 ', dataBack, index, dataBack.data && dataBack.data.success === 1 && dataBack.data.results && dataBack.data.results.odds && dataBack.data.results.odds['18_3'] && dataBack.data.results.odds['18_3'][0])
 
-									// 		if (dataBack.data && dataBack.data.success === 1 && dataBack.data.results && dataBack.data.results.odds && dataBack.data.results.odds['18_3'] && dataBack.data.results.odds['18_3'][0]) {
-									// 			convertedObject.bet365Odds = dataBack.data.results.odds['18_3'][0]
-									// 			console.log('Databack:1 ', dataBack.data, index)
-									// 			if (detailedData.league.cc === 'us') {
-									// 				filledUSAData.push(convertedObject);
-									// 			} else {
-									// 				filledWorldData.push(convertedObject);
-									// 			}
-									// 		}
-									// 	})
-									// 	.catch(e => {
-									// 		console.error('Error getting bet365 odds', e)
-									// 	})
-									// }
-									if (detailedData.league.cc === 'us') {
-										filledUSAData.push(convertedObject);
-									} else {
-										filledWorldData.push(convertedObject);
-									}
-								}
-								if (fetchedData.length - 1 === index) {
-									// came to the end, can push data to state
-									console.log('Filled data: ', filledWorldData, filledUSAData)
-									// If USA country then push in separate array.
-									setAllWorldMatches(filledWorldData);
-									setAllUSAMatches(filledUSAData);
-									setLastRefresh(moment().format('YYYY-MM-DD hh:mm:ss'))
-								}
-							}
-						})
-						.catch(err => console.error('Error getting detaild data', err));
-				})
+	const fetchInplayEvents = async () => {
+		try {
+			const response = await axios.get(`https://your-vercel-app-url.vercel.app/api/proxy?apiUrl=https://api.b365api.com/v3/events/inplay&sport_id=18`);
+			// const response = await axios.get('https://api.b365api.com/v3/events/inplay?sport_id=18&token=154761-g9sYpS0kbXfwrV');
+			return response.data.results;
+		} catch (error) {
+			console.error('Error fetching inplay events:', error);
+			return [];
+		}
+	};
+	
+	const fetchEventDetails = async (eventId) => {
+		try {
+			const response = await axios.get(`https://your-vercel-app-url.vercel.app/api/proxy?apiUrl=https://api.b365api.com/v1/event/view&event_id=${eventId}`);
+			// const response = await axios.get(`https://api.b365api.com/v1/event/view?token=154761-g9sYpS0kbXfwrV&event_id=${eventId}`);
+			return response.data.results[0];
+		} catch (error) {
+			console.error('Error fetching event details:', error);
+			return null;
+		}
+	};
+
+	const processEventData = (detailedData) => {
+		const scoreArr = detailedData.ss.split('-');
+		return {
+			homeName: detailedData.home.name,
+			awayName: detailedData.away.name,
+			country: detailedData.league.cc,
+			score: detailedData.ss,
+			scoreDiff: parseInt(scoreArr[0]) - parseInt(scoreArr[1]),
+			scoreTotal: parseInt(scoreArr[0]) + parseInt(scoreArr[1]),
+			bet365Odds: { handicap: null },
+			bet365OddsHandicap: null,
+			timer: detailedData.timer,
+			timerString: `${detailedData.timer.tm}min ${detailedData.timer.ts}sec`,
+			stats: detailedData.stats,
+			id: detailedData.id,
+			key: detailedData.id,
+			quarter: `${detailedData.timer.q}`,
+			foulsHome: detailedData.stats.fouls[0],
+			foulsAway: detailedData.stats.fouls[1],
+			points2Home: detailedData.stats['2points'][0],
+			points2Away: detailedData.stats['2points'][1],
+			points3Home: detailedData.stats['3points'][0],
+			points3Away: detailedData.stats['3points'][1],
+			freeThrowsHome: detailedData.stats.free_throws[0],
+			freeThrowsAway: detailedData.stats.free_throws[1],
+			freeThrowsRateHome: detailedData.stats.free_throws_rate[0],
+			freeThrowsRateAway: detailedData.stats.free_throws_rate[1],
+			timeoutsHome: detailedData.stats.time_outs[0],
+			timeoutsAway: detailedData.stats.time_outs[1],
+		};
+	};
+
+	const updateData = async () => {
+		const inplayEvents = await fetchInplayEvents();
+	
+		const eventDetailPromises = inplayEvents.map((event) => fetchEventDetails(event.id));
+		const eventDetails = await Promise.all(eventDetailPromises);
+	
+		const filledWorldData = [];
+		const filledUSAData = [];
+	
+		eventDetails.forEach((detailedData) => {
+			if (
+				detailedData &&
+				Object.keys(detailedData.stats).length &&
+				Object.keys(detailedData.timer).length &&
+				detailedData.league.id !== '25067' &&
+				detailedData.league.id !== '23105'
+			) {
+				const convertedObject = processEventData(detailedData);
+	
+				if (detailedData.league.cc === 'us') {
+					filledUSAData.push(convertedObject);
+				} else {
+					filledWorldData.push(convertedObject);
+				}
 			}
-		})
-		.catch(e => console.error('Have error: ', e))
-	}
+		});
+	
+		console.log('Filled data: ', filledWorldData, filledUSAData);
+	
+		setAllWorldMatches(filledWorldData);
+		setAllUSAMatches(filledUSAData);
+		setLastRefresh(moment().format('YYYY-MM-DD hh:mm:ss'));
+	};
+	
+ 
+	// const updateData = async () => {
+	// 	// axios.get(`${process.env.REACT_APP_API_URL}/api/proxy?apiUrl=https://api.b365api.com/v3/events/inplay&sport_id=18`)
+	// 	axios.get('https://api.b365api.com/v3/events/inplay?sport_id=18&token=154761-g9sYpS0kbXfwrV')
+	// 	.then((res) => {
+	// 		if (res?.data?.results) {
+	// 			const filledWorldData = [];
+	// 			const filledUSAData = [];
+	// 			const fetchedData = res?.data?.results;
+	// 			fetchedData.forEach((singleData, index) => {
+	// 				// axios.get(`${process.env.REACT_APP_API_URL}/api/proxy?apiUrl=https://api.b365api.com/v1/event/view&event_id=${singleData.id}`)
+	// 				axios.get(`https://api.b365api.com/v1/event/view?token=154761-g9sYpS0kbXfwrV&event_id=${singleData.id}`)
+	// 					.then((eventData) => {
+	// 						if (eventData?.data?.results[0]) {
+	// 							const detailedData = eventData.data.results[0];
+	// 							if (Object.keys(detailedData.stats).length && Object.keys(detailedData.timer).length && detailedData.league.id !== '25067' && detailedData.league.id !== '23105') {
+	// 								console.log('Adding for id: ', detailedData.id, detailedData)
+	// 								const scoreArr = (detailedData.ss).split('-')
+	// 								const convertedObject = {
+	// 									homeName: detailedData.home.name,
+	// 									awayName: detailedData.away.name,
+	// 									country: detailedData.league.cc,
+	// 									score: detailedData.ss,
+	// 									scoreDiff: parseInt(scoreArr[0]) - parseInt(scoreArr[1]),
+	// 									scoreTotal: parseInt(scoreArr[0]) + parseInt(scoreArr[1]),
+	// 									bet365Odds: { handicap: null },
+	// 									bet365OddsHandicap: null,
+	// 									timer: detailedData.timer,
+	// 									timerString: `${detailedData.timer.tm}min ${detailedData.timer.ts}sec`,
+	// 									//  ${detailedData.timer.tt} -> check what it means
+	// 									stats: detailedData.stats,
+	// 									id: detailedData.id,
+	// 									key: detailedData.id,
+	// 									// quarter: `${detailedData.timer.q} ${(detailedData.extra.numberofperiods !== '4' ? '2H' : '')}`,
+	// 									quarter: `${detailedData.timer.q}`,
+	// 									foulsHome: detailedData.stats.fouls[0],
+	// 									foulsAway: detailedData.stats.fouls[1],
+	// 									points2Home: detailedData.stats['2points'][0],
+	// 									points2Away: detailedData.stats['2points'][1],
+	// 									points3Home: detailedData.stats['3points'][0],
+	// 									points3Away: detailedData.stats['3points'][1],
+	// 									freeThrowsHome: detailedData.stats.free_throws[0],
+	// 									freeThrowsAway: detailedData.stats.free_throws[1],
+	// 									freeThrowsRateHome: detailedData.stats.free_throws_rate[0],
+	// 									freeThrowsRateAway: detailedData.stats.free_throws_rate[1],
+	// 									timeoutsHome: detailedData.stats.time_outs[0],
+	// 									timeoutsAway: detailedData.stats.time_outs[1],
+	// 									// numberofperiods: detailedData.extra.numberofperiods,
+	// 								}
+	// 								if (detailedData.league.cc === 'us') {
+	// 									filledUSAData.push(convertedObject);
+	// 								} else {
+	// 									filledWorldData.push(convertedObject);
+	// 								}
+	// 							}
+	// 							if (fetchedData.length - 1 === index) {
+	// 								// came to the end, can push data to state
+	// 								console.log('Filled data: ', filledWorldData, filledUSAData)
+	// 								// If USA country then push in separate array.
+	// 								setAllWorldMatches(filledWorldData);
+	// 								setAllUSAMatches(filledUSAData);
+	// 								setLastRefresh(moment().format('YYYY-MM-DD hh:mm:ss'))
+	// 							}
+	// 						}
+	// 					})
+	// 					.catch(err => console.error('Error getting detaild data', err));
+	// 			})
+	// 		}
+	// 	})
+	// 	.catch(e => console.error('Have error: ', e))
+	// }
 
 	const totalWorldMatches = allWorldMatches.length
 	const totalUSAMatches = allUSAMatches.length
 
-	// const updateObjectInArray = (arr, updatedObj) => {
-	// 	return arr.map(obj => {
-	// 		if (obj.id === updatedObj.id) {
-	// 			return updatedObj;
-	// 		}
-	// 		return obj;
-	// 	});
-	// }
-
-	const getBet365Odds = (stateMatches) => {
-		const matchesWithOdds = [];
-		if (stateMatches.length > 0) {
-			stateMatches.forEach((stateMatch) => {
-				axios.get(`https://api.b365api.com/v2/event/odds?token=154761-g9sYpS0kbXfwrV&event_id=${stateMatch.id}`)
-				.then((dataBack) => {
-					console.log('DATA BACK: ', dataBack)
-					if (dataBack.data && dataBack.data.success === 1 && dataBack.data.results && dataBack.data.results.odds && dataBack.data.results.odds['18_3'] && dataBack.data.results.odds['18_3'][0]) {
-						console.log('DATABACK odds: ', dataBack.data.results.odds['18_3'][0])
-						const updatedStateMatch = { ...stateMatch, bet365Odds: dataBack.data.results.odds['18_3'][0], bet365OddsHandicap: dataBack.data.results.odds['18_3'][0].handicap }
-						// convertedObject.bet365Odds = dataBack.data.results.odds['18_3'][0]
-						// Update state with new values matchesToShow
-						matchesWithOdds.push(updatedStateMatch);
-						// setMatchesToShow(updateObjectInArray(matchesToShow, updatedStateMatch))
-					}
-				})
-				.catch(e => {
-					console.error('Error getting bet365 odds', e)
-				})
-			})
-			// console.log('matchesWithOdds: ', matchesWithOdds)
-			setTimeout(() => {
-			setMatchesToShow(matchesWithOdds)
-			}, 3000)
+	const fetchBet365Odds = async (matchId) => {
+		try {
+			const response = await axios.get(`https://api.b365api.com/v2/event/odds?token=154761-g9sYpS0kbXfwrV&event_id=${matchId}`);
+			if (
+				response.data &&
+				response.data.success === 1 &&
+				response.data.results &&
+				response.data.results.odds &&
+				response.data.results.odds['18_3'] &&
+				response.data.results.odds['18_3'][0]
+			) {
+				return response.data.results.odds['18_3'][0];
+			}
+			return null;
+		} catch (error) {
+			console.error('Error getting bet365 odds:', error);
+			return null;
 		}
-	}
+	};
+
+	const getBet365Odds = async (stateMatches) => {
+		if (stateMatches.length > 0) {
+			const oddsPromises = stateMatches.map((match) => fetchBet365Odds(match.id));
+			const oddsResults = await Promise.all(oddsPromises);
+	
+			const matchesWithOdds = stateMatches.map((match, index) => {
+				if (oddsResults[index]) {
+					return { ...match, bet365Odds: oddsResults[index], bet365OddsHandicap: oddsResults[index].handicap };
+				}
+				return match;
+			});
+	
+			setMatchesToShow(matchesWithOdds);
+		}
+	};
+
+	// const getBet365Odds = (stateMatches) => {
+	// 	const matchesWithOdds = [];
+	// 	if (stateMatches.length > 0) {
+	// 		stateMatches.forEach((stateMatch) => {
+	// 			axios.get(`https://api.b365api.com/v2/event/odds?token=154761-g9sYpS0kbXfwrV&event_id=${stateMatch.id}`)
+	// 			.then((dataBack) => {
+	// 				console.log('DATA BACK: ', dataBack)
+	// 				if (dataBack.data && dataBack.data.success === 1 && dataBack.data.results && dataBack.data.results.odds && dataBack.data.results.odds['18_3'] && dataBack.data.results.odds['18_3'][0]) {
+	// 					console.log('DATABACK odds: ', dataBack.data.results.odds['18_3'][0])
+	// 					const updatedStateMatch = { ...stateMatch, bet365Odds: dataBack.data.results.odds['18_3'][0], bet365OddsHandicap: dataBack.data.results.odds['18_3'][0].handicap }
+	// 					// Update state with new values matchesToShow
+	// 					matchesWithOdds.push(updatedStateMatch);
+	// 					// setMatchesToShow(updateObjectInArray(matchesToShow, updatedStateMatch))
+	// 				}
+	// 			})
+	// 			.catch(e => {
+	// 				console.error('Error getting bet365 odds', e)
+	// 			})
+	// 		})
+	// 		// console.log('matchesWithOdds: ', matchesWithOdds)
+	// 		setTimeout(() => {
+	// 		setMatchesToShow(matchesWithOdds)
+	// 		}, 3000)
+	// 	}
+	// }
 
 	const handleChange = (value) => {
 		setFiltersSelected(value)
