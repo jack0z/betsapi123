375eb5a29835481f010fc704aca8939425d1b1fc
polytropo <primoz969@gmail.com>
2023-05-09 11:39:00 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -5,6 +5,7 @@ module.exports = async (req, res) => {
   const newUrl = new URL(apiUrl);
 
   newUrl.searchParams.set('token', '154761-g9sYpS0kbXfwrV');
+  console.log('(newUrl.toString', newUrl.toString());
   try {
     const response = await axios.get(newUrl.toString());
     res.status(200).json(response.data);
